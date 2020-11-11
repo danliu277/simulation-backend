ebf30992c1a1ac976196e5e256efd2c64de44b7f
flatironschool <danliu277@gmail.com>
2020-11-11 15:31:39 -0500

    Use nokogiri in progress

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+doc = Nokogiri::HTML(open("https://pokemon.alexonsager.net/"))
+5.times do
+     
+    puts doc.at_css('[id="pk_img"]').attributes["src"]
+end
